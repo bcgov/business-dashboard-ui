09688f4b5ddeb0ae791d46cb9de8eeac4ba4a858
Patrick Wang <root@NI502169.idir.BCGOV>
2024-06-21 12:47:02 -0700

    fix AddressDisplay unit test

--- tests/components/bcros/BcrosAddressDisplay.spec.ts
+++ tests/components/bcros/BcrosAddressDisplay.spec.ts
@@ -1,12 +1,13 @@
 import { describe, expect, it } from 'vitest'
 import { mount } from '@vue/test-utils'
+import { getName } from 'country-list'
 
 import { BcrosAddressDisplay } from '#components'
 
 describe('AddressDisplay tests', () => {
   const addressFilled: AddressI = {
     addressCity: 'Vancouver',
-    addressCountry: 'Canada',
+    addressCountry: 'CA',
     addressRegion: 'BC',
     locationDescription: 'Cool house',
     postalCode: 'V1L 4T7',
@@ -16,14 +17,14 @@ describe('AddressDisplay tests', () => {
 
   const addressPartial: Partial<AddressI> = {
     addressCity: 'Victoria',
-    addressCountry: 'Canada',
+    addressCountry: 'CA',
     addressRegion: 'BC',
     postalCode: 'V1K 5T8',
     streetAddress: '452 Waffles Street'
   }
 
   const addressMissingParts: Partial<AddressI> = {
-    addressCountry: 'Canada',
+    addressCountry: 'CA',
     addressRegion: '',
     locationDescription: 'Find me if you can',
     postalCode: null,
@@ -39,7 +40,7 @@ describe('AddressDisplay tests', () => {
     expect(addressLines.at(0).text()).toBe(addressFilled.streetAddress)
     expect(addressLines.at(1).text()).toBe(addressFilled.streetAdditional)
     expect(addressLines.at(2).text()).toBe('Vancouver BC V1L 4T7')
-    expect(addressLines.at(3).text()).toBe(addressFilled.addressCountry)
+    expect(addressLines.at(3).text()).toBe(getName(addressFilled.addressCountry))
     const locDesc = wrapper.find('[data-cy=location-description]')
     expect(locDesc.exists()).toBe(true)
     expect(locDesc.find('.title').text()).toBe('Location Description')
@@ -57,7 +58,7 @@ describe('AddressDisplay tests', () => {
     expect(addressLines.length).toBe(3)
     expect(addressLines.at(0).text()).toBe(addressPartial.streetAddress)
     expect(addressLines.at(1).text()).toBe('Victoria BC V1K 5T8')
-    expect(addressLines.at(2).text()).toBe(addressPartial.addressCountry)
+    expect(addressLines.at(2).text()).toBe(getName(addressFilled.addressCountry))
     expect(wrapper.find('[data-cy=location-description]').exists()).toBe(false)
 
     wrapper.unmount()
@@ -70,7 +71,7 @@ describe('AddressDisplay tests', () => {
     expect(wrapper.find('[data-cy=address-display]').exists()).toBe(true)
     const addressLines = wrapper.findAll('[data-cy=address-line]')
     expect(addressLines.length).toBe(1)
-    expect(addressLines.at(0).text()).toBe(addressMissingParts.addressCountry)
+    expect(addressLines.at(0).text()).toBe(getName(addressFilled.addressCountry))
     const locDesc = wrapper.find('[data-cy=location-description]')
     expect(locDesc.exists()).toBe(true)
     expect(locDesc.find('.title').text()).toBe('Location Description')
