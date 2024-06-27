5814b8cf83caa3d76da3f582cf301a8a17d2a702
BrandonSharratt <brandon@highwaythreesolutions.com>
2024-06-27 14:39:01 -0700

    initial pass at alerts

---
+++ src/components/bcros/Alerts.vue
@@ -0,0 +1,17 @@
+<template>
+  <div data-cy="alert-display">
+    <div
+      v-for="alert, i in alerts"
+      :key="`alert-${i}`"
+      data-cy="alert-line"
+    >
+      <BcrosAlert :alert="alert" :contact="contact" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps<{ alerts: Array<Partial<AlertI>>, contact: boolean }>()
+
+</script>
