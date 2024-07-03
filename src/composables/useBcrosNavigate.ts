75c2762ef0a445bb770da5d283c932d28c039277
Kial Jinnah <kialj876@gmail.com>
2024-07-03 12:12:00 -0700

    common redirect update
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- src/composables/useBcrosNavigate.ts
+++ src/composables/useBcrosNavigate.ts
@@ -3,15 +3,18 @@ export const useBcrosNavigate = () => {
   const account = useBcrosAccount()
 
   /** Redirect to the given URL with necessary BCROS args */
-  function redirect (url: string) {
+  function redirect (url: string, params?: { [key: string]: string }, target = '_self') {
     // get account id and set in params
     const redirectURL = new URL(url)
     const accountId = account.currentAccount.id
     if (accountId) {
-      redirectURL.searchParams.append('accountid', accountId)
+      redirectURL.searchParams.append('accountid', accountId.toString())
+    }
+    for (const [key, value] of Object.entries(params)) {
+      redirectURL.searchParams.append(key, value)
     }
     // assume URL is always reachable
-    window.location.assign(redirectURL.href)
+    window.open(redirectURL, target)
   }
 
   // common redirects
