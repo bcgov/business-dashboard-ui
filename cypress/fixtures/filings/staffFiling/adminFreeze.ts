babae9395d9b4a340db3c400eaa18e8007e81704
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2024-10-08 10:49:35 -0700

    23439; 23197 - Bug fixing

---
+++ cypress/fixtures/filings/staffFiling/adminFreeze.ts
@@ -0,0 +1,59 @@
+export const adminFreeze = {
+  availableOnPaperOnly: false,
+  businessIdentifier: 'CP1002605',
+  commentsCount: 0,
+  commentsLink:
+    'https://legal-api-dev.apps.silver.devops.gov.bc.ca/api/v2/businesses/CP1002605/filings/151402/comments',
+  data: {
+    adminFreeze: {
+      freeze: false
+    },
+    applicationDate: '2024-10-08T00:23:55.027171+00:00',
+    legalFilings: [
+      'adminFreeze'
+    ]
+  },
+  displayLedger: false,
+  displayName: 'Admin Freeze',
+  documentsLink:
+    'https://legal-api-dev.apps.silver.devops.gov.bc.ca/api/v2/businesses/CP1002605/filings/151402/documents',
+  effectiveDate: 'Tue, 08 Oct 2024 00:23:56 GMT',
+  filingId: 151402,
+  filingLink: 'https://legal-api-dev.apps.silver.devops.gov.bc.ca/api/v2/businesses/CP1002605/filings/151402',
+  isFutureEffective: false,
+  name: 'adminFreeze',
+  paymentStatusCode: 'COMPLETED',
+  status: 'COMPLETED',
+  submittedDate: 'Tue, 08 Oct 2024 00:23:55 GMT',
+  submitter: 'Registry Staff'
+}
+
+export const adminFreezeWithDisplayLedgerTrue = {
+  availableOnPaperOnly: false,
+  businessIdentifier: 'CP1002605',
+  commentsCount: 0,
+  commentsLink:
+    'https://legal-api-dev.apps.silver.devops.gov.bc.ca/api/v2/businesses/CP1002605/filings/151402/comments',
+  data: {
+    adminFreeze: {
+      freeze: false
+    },
+    applicationDate: '2024-10-08T00:22:55.027171+00:00',
+    legalFilings: [
+      'adminFreeze'
+    ]
+  },
+  displayLedger: true,
+  displayName: 'Admin Freeze',
+  documentsLink:
+    'https://legal-api-dev.apps.silver.devops.gov.bc.ca/api/v2/businesses/CP1002605/filings/151402/documents',
+  effectiveDate: 'Tue, 08 Oct 2024 00:23:56 GMT',
+  filingId: 151402,
+  filingLink: 'https://legal-api-dev.apps.silver.devops.gov.bc.ca/api/v2/businesses/CP1002605/filings/151402',
+  isFutureEffective: false,
+  name: 'adminFreeze',
+  paymentStatusCode: 'COMPLETED',
+  status: 'COMPLETED',
+  submittedDate: 'Tue, 08 Oct 2024 00:23:55 GMT',
+  submitter: 'Registry Staff'
+}
