f27642956d8bf3693d8d9134e99dc1074de4ada2
Peinan Wang <peinanwang@RiddleMBP.local>
2024-10-09 11:58:56 -0700

    update menu options

--- src/components/bcros/filing/addStaffFiling/Index.vue
+++ src/components/bcros/filing/addStaffFiling/Index.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import { FilingTypes } from '@bcrs-shared-components/enums'
+
 const t = useNuxtApp().$i18n.t
 
 interface MenuActionItem extends DropdownItem {
@@ -7,9 +8,10 @@ interface MenuActionItem extends DropdownItem {
   datacy: string
 }
 
+const filings = useBcrosFilings()
 const business = useBcrosBusiness()
 const { currentBusiness } = storeToRefs(business)
-const { goToBusinessDashboard } = useBcrosNavigate()
+const { goToBusinessDashboard, goToEditPage, goToCreatePage } = useBcrosNavigate()
 
 const openFreezeUnfreezeModal = ref(false)
 const openRegistrarNotationModal = ref(false)
@@ -17,6 +19,34 @@ const openRegistrarOrderModal = ref(false)
 const openCourtOrderModal = ref(false)
 const openDissolutionModal = ref(false)
 const openPutBackOnModal = ref(false)
+
+// Create a restoration filing and navigate to the appropriate page
+const restoreCompany = async (restorationType: FilingSubTypeE = null) => {
+  // create restoration filing
+  const response: any = await filings.createFiling(
+    currentBusiness.value,
+    FilingTypes.RESTORATION,
+    { type: restorationType }
+  )
+
+  if (response.error?.value) {
+    console.error(response.error.value)
+    return
+  }
+
+  const filingId = response.data?.value.filing.header.filingId + 1
+
+  // navigate to Edit UI for limited restoration extension filing
+  // navigate to Edit UI for limited restoration to full filing
+  // navigate to Create UI for full or limited restoration filing
+  if (restorationType === FilingSubTypeE.LIMITED_RESTORATION_EXTENSION) {
+    goToEditPage(`/${currentBusiness.value.identifier}/limitedRestorationExtension`, { 'restoration-id': filingId })
+  } else if (restorationType === FilingStatusE.LIMITED_RESTORATION_TO_FULL) {
+    goToEditPage(`/${currentBusiness.value.identifier}/limitedRestorationToFull`, { 'restoration-id': filingId })
+  } else {
+    goToCreatePage('/restoration-business-name', { id: currentBusiness.value.identifier })
+  }
+}
 
 const allActions: ComputedRef<Array<MenuActionItem>> = computed(() => {
   return [
@@ -41,12 +71,35 @@ const allActions: ComputedRef<Array<MenuActionItem>> = computed(() => {
       label: t('label.filing.staffFilingOptions.courtOrder'),
       click: () => { openCourtOrderModal.value = true }
     },
+    { // <!-- Record Conversion -->
+      showButton: business.isEntityFirm,
+      disabled: !business.isAllowedToFile(FilingTypes.CONSENT_CONTINUATION_OUT),
+      datacy: 'record-conversion',
+      label: t('label.filing.staffFilingOptions.recordConversion'),
+      click: () => {
+        goToEditPage(`/${currentBusiness.value.identifier}/conversion`)
+      }
+    },
     { // <!-- Admin Dissolution -->
-      showButton: business.isAllowedToFile(FilingTypes.DISSOLUTION),
-      disabled: false,
+      showButton: currentBusiness.value?.state !== BusinessStateE.HISTORICAL,
+      disabled: !business.isAllowedToFile(FilingTypes.DISSOLUTION),
       datacy: 'dissolution',
       label: t('label.filing.staffFilingOptions.dissolution'),
       click: () => { openDissolutionModal.value = true }
+    },
+    { // <!-- Restore Company  -->
+      showButton: currentBusiness.value?.state !== BusinessStateE.HISTORICAL,
+      disabled: !business.isAllowedToFile(FilingTypes.RESTORATION),
+      datacy: 'restore',
+      label: t('label.filing.staffFilingOptions.restoreCompany'),
+      click: () => { restoreCompany() }
+    },
+    { // <!-- Put Back On -->
+      showButton: business.isAllowedToFile(FilingTypes.PUT_BACK_ON),
+      disabled: false,
+      datacy: 'put-back-on',
+      label: t('label.filing.staffFilingOptions.putBackOn'),
+      click: () => { openPutBackOnModal.value = true }
     },
     { // <!-- Admin Freeze/Unfreeze -->
       showButton: business.isAllowedToFile(FilingTypes.ADMIN_FREEZE),
@@ -57,34 +110,56 @@ const allActions: ComputedRef<Array<MenuActionItem>> = computed(() => {
         : t('label.filing.staffFilingOptions.adminUnfreeze'),
       click: () => { openFreezeUnfreezeModal.value = true }
     },
-    { // <!-- Put Back On -->
-      showButton: business.isAllowedToFile(FilingTypes.PUT_BACK_ON),
-      disabled: false,
-      datacy: 'put-back-on',
-      label: t('label.filing.staffFilingOptions.putBackOn'),
-      click: () => { openPutBackOnModal.value = true }
-    },
     { // <!-- Consent to Amalgamate Out -->
-      showButton: business.isAllowedToFile(FilingTypes.CONSENT_AMALGAMATION_OUT),
-      disabled: false,
+      showButton: currentBusiness.value?.state !== BusinessStateE.HISTORICAL,
+      disabled: !business.isAllowedToFile(FilingTypes.CONSENT_AMALGAMATION_OUT),
       datacy: 'consent-to-amalgamate-out',
-      label: t('button.tombstone.menuAction.consentToAmalgamateOut'),
+      label: t('label.filing.staffFilingOptions.consentToAmalgamateOut'),
       click: () => {
         goToBusinessDashboard(`/${currentBusiness.value.identifier}/consent-amalgamation-out'`, { filingId: '0' })
       }
     },
+    { // <!-- Amalgamate -->
+      showButton: currentBusiness.value?.state !== BusinessStateE.HISTORICAL,
+      disabled: !business.isAllowedToFile(FilingTypes.AMALGAMATION_OUT),
+      datacy: 'amalgamate-out',
+      label: t('label.filing.staffFilingOptions.amalgamateOut'),
+      click: () => {
+        goToBusinessDashboard(`/${currentBusiness.value.identifier}/amalgamation-out'`, { filingId: '0' })
+      }
+    },
     { // <!-- Consent to Continue Out -->
-      showButton: business.isAllowedToFile(FilingTypes.CONSENT_CONTINUATION_OUT),
-      disabled: false,
+      showButton: currentBusiness.value?.state !== BusinessStateE.HISTORICAL,
+      disabled: !business.isAllowedToFile(FilingTypes.CONSENT_CONTINUATION_OUT),
       datacy: 'consent-to-continue-out',
-      label: t('button.tombstone.menuAction.consentToContinueOut'),
+      label: t('label.filing.staffFilingOptions.consentToContinueOut'),
       click: () => {
         goToBusinessDashboard(`/${currentBusiness.value.identifier}/consent-continuation-out`, { filingId: '0' })
       }
+    },
+    { // <!-- Continue Out -->
+      showButton: currentBusiness.value?.state !== BusinessStateE.HISTORICAL,
+      disabled: !business.isAllowedToFile(FilingTypes.CONTINUATION_OUT),
+      datacy: 'continue-out',
+      label: t('label.filing.staffFilingOptions.continueOut'),
+      click: () => {
+        goToBusinessDashboard(`/${currentBusiness.value.identifier}/consent-continuation-out`, { filingId: '0' })
+      }
+    },
+    { // <!-- Extend Limited Restoration  -->
+      showButton: currentBusiness.value?.state !== BusinessStateE.HISTORICAL,
+      disabled: !business.isAllowedToFile(FilingTypes.RESTORATION, FilingSubTypeE.LIMITED_RESTORATION_EXTENSION),
+      datacy: 'limit-restore',
+      label: t('label.filing.staffFilingOptions.extendLimitedRestoration'),
+      click: () => { restoreCompany(FilingSubTypeE.LIMITED_RESTORATION_EXTENSION) }
+    },
+    { // <!-- Convert to Full Restoration  -->
+      showButton: business.isAllowedToFile(FilingTypes.RESTORATION, FilingSubTypeE.LIMITED_RESTORATION_TO_FULL),
+      disabled: false,
+      datacy: 'full-restore',
+      label: t('label.filing.staffFilingOptions.fullRestoration'),
+      click: () => { restoreCompany(FilingSubTypeE.LIMITED_RESTORATION_TO_FULL) }
     }
-    // amalgamate
-    // continue out
-    // restore
   ]
 })
 
@@ -136,6 +211,7 @@ const actions: ComputedRef<Array<Array<MenuActionItem>>> = computed(() => {
         <UButton
           variant="ghost"
           :label="item.label"
+          :disabled="item.disabled"
           :data-cy="item.datacy"
           class="w-full text-nowrap"
           @click="item.click"
