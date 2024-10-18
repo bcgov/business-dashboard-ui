52004154ac33961a55363b3ae17350a8d84a06ab
BrandonSharratt <brandon@highwaythreesolutions.com>
2024-10-18 12:17:28 -0700

    fix lint and tests

--- cypress/e2e/components/tombstone/menu-options.cy.ts
+++ cypress/e2e/components/tombstone/menu-options.cy.ts
@@ -31,12 +31,12 @@ context('Business tombstone - action buttons in the dropdown menu', () => {
   it('Verify the action buttons in the dropdown menu', () => {
     Cypress.on('uncaught:exception', (error: Error) => {
       // returning false here prevents Cypress from failing the test for the postMessage error that happens sometimes
-      console.error('Caught error', error);
+      console.error('Caught error', error)
       if (error.stack?.includes('PrimaryOriginCommunicator.toSource')) {
-        return false;
+        return false
       }
-      return true;
-    });
+      return true
+    })
     // Intercept the request for Continuation Out, Request AGM Extension, Request AGM Location Change, and Amalgamate
     cy.intercept('GET', '**/**/consent-continuation-out?**filingId=0**').as('goToContinuationOut')
     cy.intercept('GET', '**/**/agm-extension?**filingId=0**').as('goToAgmExtension')
@@ -90,7 +90,7 @@ context('Business tombstone - action buttons in the dropdown menu', () => {
       .find('[data-cy="bcros-dialog-title"]')
       .should('have.text', 'Voluntary Dissolution')
 
-    cy.get('[data-cy="dissolution-button"]').click().wait('@fileDissolution')//.wait('@goToDissolution')
+    cy.get('[data-cy="dissolution-button"]').click().wait('@fileDissolution') // Cypress doesn't like to wait for CORS urls .wait('@goToDissolution')
 
     // open the dissolution confirm dialog for 'Dissolution' for a SP company
     cy.visitBusinessDashFor('businessInfo/sp/active.json')
@@ -102,6 +102,6 @@ context('Business tombstone - action buttons in the dropdown menu', () => {
       .find('[data-cy="bcros-dialog-title"]')
       .should('have.text', 'Dissolution')
 
-    cy.get('[data-cy="dissolution-button"]').click().wait('@fileDissolution')//.wait('@goToDissolution')
+    cy.get('[data-cy="dissolution-button"]').click().wait('@fileDissolution')// Cypress doesn't like to wait for CORS urls .wait('@goToDissolution')
   })
 })
