857f0f0eff76256bea42ed8adfe0166ef89c35e6
BrandonSharratt <brandon@highwaythreesolutions.com>
2024-10-18 07:43:10 -0700

    revert test change

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    requestTimeout: 10000, //double the default timeout
     setupNodeEvents (on) {
       on('task', {
         log (message) {
