7ebf97aecce2dc23acc21e203c017e3b409116d8
BrandonSharratt <brandon@highwaythreesolutions.com>
2024-10-18 07:37:58 -0700

    try to increae cypress request timeout

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+    requestTimeout: 10000, //double the default timeout
     setupNodeEvents (on) {
       on('task', {
         log (message) {
