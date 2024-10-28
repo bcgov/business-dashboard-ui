486cc39050c291b963966a6d6e23c6f82fbc08d1
BrandonSharratt <brandon@highwaythreesolutions.com>
2024-10-28 14:20:11 -0700

    fix dialog title, remove red border from correction todo and disable change when a correction filing is in todo

--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -186,8 +186,23 @@ const pendingAddress = computed(() => {
   return false
 })
 
-const isChangeAddressDisabled = computed(() => business.currentBusiness.adminFreeze || pendingAddress.value)
-const isChangeDirectorDisabled = computed(() => business.currentBusiness.adminFreeze)
+const hasProgressCorrections = computed(() => {
+  if (filings && filings.value && filings.value.length > 0) {
+    filings.value.filter((filing) => {
+      return filing.name === FilingTypes.CORRECTION &&
+        (filing.status === FilingStatusE.DRAFT ||
+        filing.status === FilingStatusE.PENDING ||
+        filing.status === FilingStatusE.PENDING_CORRECTION)
+    })
+  }
+  return false
+})
+
+const isChangeAddressDisabled = computed(() => {
+  return business.currentBusiness.adminFreeze || pendingAddress.value || hasProgressCorrections
+})
+
+const isChangeDirectorDisabled = computed(() => business.currentBusiness.adminFreeze || hasProgressCorrections)
 const showChangeOfAddress = ref(false)
 
 const setChangeOfAddress = (show: boolean) => {
