c8705332ba6ce312f7e1e3072ebaededd55f84ee
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2024-12-06 14:56:55 -0800

    fix: issue when there are multiple accountid query params.

--- src/middleware/02.setupAuth.global.ts
+++ src/middleware/02.setupAuth.global.ts
@@ -4,9 +4,13 @@ export default defineNuxtRouteMiddleware(async (to) => {
     // keycloak redirects with the error param when not logged in (nuxt/keycloak issue)
     //   - removing ^ condition will cause an infinite loop of keycloak redirects when not authenticated
     const { kcURL, kcRealm, kcClient } = useRuntimeConfig().public
+    let accountNumber = to?.params?.accountid || to?.query?.accountid || undefined
+    if (Array.isArray(accountNumber)) {
+      accountNumber = accountNumber[0]
+    }
     await useBcrosAuth().setupAuth(
       { url: kcURL, realm: kcRealm, clientId: kcClient },
-      to.params.accountid as string || to.query.accountid as string
+      accountNumber
     )
   }
   // For cypress tests. NOTE: all api calls will need to be intercepted/stubbed
