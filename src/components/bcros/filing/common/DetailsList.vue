d2b070f359fec93a189df66f35a03781f1e77dc2
Rajandeep <rajandeep.kaur@gov.bc.ca>
2025-01-20 12:57:08 -0800

    added css

--- src/components/bcros/filing/common/DetailsList.vue
+++ src/components/bcros/filing/common/DetailsList.vue
@@ -52,7 +52,7 @@ const showCommentDialog = (show: boolean) => {
             ({{ apiToPacificDateTime(comment.timestamp) }})
           </div>
           <div class="body-2">
-            <div class="pre-line whitespace-pre">
+            <div class="whitespace-pre-line break-words">
               {{ comment.comment }}
             </div>
           </div>
