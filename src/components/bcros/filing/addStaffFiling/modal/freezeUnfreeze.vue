20db21979c7ac50cdb4247be1dde6cd7b836b393
Rajandeep Kaur <144159721+Rajandeep98@users.noreply.github.com>
2025-02-27 10:46:22 -0800

    25518 - Business Dashboard: retries not restarting after Freeze or Unfreeze (#152)
    * 25518 freeze unfreeze slim calls
    
    * 25388 - todo delays
    
    * removing other tt work

--- src/components/bcros/filing/addStaffFiling/modal/freezeUnfreeze.vue
+++ src/components/bcros/filing/addStaffFiling/modal/freezeUnfreeze.vue
@@ -4,7 +4,7 @@ const t = useNuxtApp().$i18n.t
 const filings = useBcrosFilings()
 const business = useBcrosBusiness()
 const { currentBusiness, currentBusinessName } = storeToRefs(business)
-const emit = defineEmits(['close'])
+const emit = defineEmits(['close', 'saved'])
 
 const open = ref(true)
 
@@ -35,6 +35,11 @@ const submitFiling = async() => {
   emit('close')
 }
 
+const handleSubmit = () => {
+  submitFiling()
+  emit('saved')
+}
+
 const submissionInProgress = ref(false)
 const filingError = ref(false)
 </script>
@@ -61,7 +66,7 @@ const filingError = ref(false)
               'text-red-500': filingError
             }"
             data-cy="submit-add-staff-filing-modal"
-            @click="submitFiling"
+            @click="handleSubmit"
           >
             {{ $t('button.dialog.file') }}
           </UButton>
