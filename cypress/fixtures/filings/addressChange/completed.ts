5d5c64a069369cc4a5e41f47234e1e27dddd658e
Severin Beauvais <severin.beauvais@gov.bc.ca>
2025-03-17 11:35:08 -0700

    - fixed unit tests

--- cypress/fixtures/filings/addressChange/completed.ts
+++ cypress/fixtures/filings/addressChange/completed.ts
@@ -1,9 +1,9 @@
 export const addressChange = {
   availableOnPaperOnly: false,
-  businessIdentifier: 'CP1002605',
+  businessIdentifier: 'BC1002605',
   commentsCount: 0,
   commentsLink:
-    'https://legal-api-dev.apps.silver.devops.gov.bc.ca/api/v2/businesses/CP1002605/filings/149681/comments',
+    'https://legal-api-dev.apps.silver.devops.gov.bc.ca/api/v2/businesses/BC1002605/filings/149681/comments',
   data: {
     applicationDate: '2024-07-02T07:00:00+00:00',
     legalFilings: [
@@ -13,14 +13,14 @@ export const addressChange = {
   displayLedger: true,
   displayName: 'Address Change',
   documentsLink:
-    'https://legal-api-dev.apps.silver.devops.gov.bc.ca/api/v2/businesses/CP1002605/filings/149681/documents',
+    'https://legal-api-dev.apps.silver.devops.gov.bc.ca/api/v2/businesses/BC1002605/filings/149681/documents',
   effectiveDate: 'Tue, 02 Jul 2024 16:30:56 GMT',
   filingId: 149681,
-  filingLink: 'https://legal-api-dev.apps.silver.devops.gov.bc.ca/api/v2/businesses/CP1002605/filings/149681',
+  filingLink: 'https://legal-api-dev.apps.silver.devops.gov.bc.ca/api/v2/businesses/BC1002605/filings/149681',
   isFutureEffective: false,
   name: 'changeOfAddress',
   paymentStatusCode: 'COMPLETED',
-  status: 'COMPLETED',
+  status: 'PAID',
   submittedDate: 'Tue, 02 Jul 2024 16:30:56 GMT',
   submitter: 'Registry Staff'
 }
