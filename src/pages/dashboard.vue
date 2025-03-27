0503f809355f592d31a0269402d730002d19fe74
Thayne Werdal <thayne.werdal@gov.bc.ca>
2025-03-27 09:12:23 -0700

    26797-Allowing director heading if proper business type
    Signed-off-by: Thayne Werdal <thayne.werdal@gov.bc.ca>

--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -25,6 +25,10 @@ const ui = useBcrosDashboardUi()
 const hasDirectors = computed(() => {
   if (currentParties.value?.parties && currentParties.value?.parties.length > 0) {
     return containRole(RoleTypeE.DIRECTOR)
+  } else if (business.isEntityCoop || business.isBaseCompany) {
+    // Want to make sure eligible corporations who have deleted
+    // all directors may add one back if required.
+    return true
   }
   return false
 })
