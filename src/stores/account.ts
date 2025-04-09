1e6abeeba8558c21b9242a77f772eeeb9f160a07
Thayne Werdal <thayne.werdal@gov.bc.ca>
2025-04-09 08:50:40 -0700

    25048-update to check query param matches what is in session storage
    Signed-off-by: Thayne Werdal <thayne.werdal@gov.bc.ca>

--- src/stores/account.ts
+++ src/stores/account.ts
@@ -155,7 +155,10 @@ export const useBcrosAccount = defineStore('bcros/account', () => {
 
   /** Set the user account list and current account */
   async function setAccountInfo (currentAccountId?: number) {
-    if (!currentAccountId) {
+    //Check if we have a currentAccountId and if it matches what is stored in our sessionStorage
+    if (!currentAccountId ||
+      (JSON.parse(sessionStorage.getItem(SessionStorageKeyE.CURRENT_ACCOUNT)) &&
+      currentAccountId !== JSON.parse(sessionStorage.getItem(SessionStorageKeyE.CURRENT_ACCOUNT)).id)) {
       // try getting id from existing session storage
       currentAccountId = JSON.parse(sessionStorage.getItem(SessionStorageKeyE.CURRENT_ACCOUNT) || '{}').id
     }
