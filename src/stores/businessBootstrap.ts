9cc8888e689470e63433afc97c452d5720dda545
Karim El Jazzar <karim.jazzar@gov.bc.ca>
2025-06-23 17:31:19 -0700

    29146 - Fixed apiURL is not defined bug

--- src/stores/businessBootstrap.ts
+++ src/stores/businessBootstrap.ts
@@ -148,8 +148,9 @@ export const useBcrosBusinessBootstrap = defineStore('bcros/businessBootstrap', 
   }
 
   const getNameRequest = async (nrNumber: string, params?: object) => {
+    const legalApiURL = useRuntimeConfig().public.legalApiURL
     return await useBcrosFetch<NameRequestI>(
-      `${apiURL}/nameRequests/${nrNumber}/validate`,
+      `${legalApiURL}/nameRequests/${nrNumber}/validate`,
       { params, dedupe: 'defer' }
     )
       .then(({ data, error }) => {
