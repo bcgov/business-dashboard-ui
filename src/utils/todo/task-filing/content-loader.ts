33f63635f0e26bdeaa99fdc980341e1846eec1e1
BrandonSharratt <brandon@highwaythreesolutions.com>
2025-09-26 13:07:02 -0700

    change approach

--- src/utils/todo/task-filing/content-loader.ts
+++ src/utils/todo/task-filing/content-loader.ts
@@ -1,5 +1,4 @@
 import { FilingNames, FilingTypes, CorpTypeCd } from '@bcrs-shared-components/enums'
-import { filingTypeToName } from './helper'
 import { nrSubtitle } from '~/utils/nr-utils'
 
 /** Get the title string for the todo item based ob the given filing TaskToDoI object */
@@ -7,6 +6,7 @@ export const getTitle = (filing: TaskToDoI, corpFullDescription: string): string
   const t = useNuxtApp().$i18n.t
   const business = useBcrosBusiness()
   const header = filing.header
+  const filingTypeToName = useFilingTypeToName().filingTypeToName
 
   let title = ''
 
@@ -85,6 +85,7 @@ export const getTitle = (filing: TaskToDoI, corpFullDescription: string): string
 /** Get the draft title string for the todo item based ob the given filing TaskToDoI object */
 export const getDraftTitle = (filing: TaskToDoI): string => {
   const t = useNuxtApp().$i18n.t
+  const filingTypeToName = useFilingTypeToName().filingTypeToName
   const header = filing.header
 
   switch (header.name) {
@@ -181,6 +182,7 @@ export const addSubtitleOrContent = (todoItem: TodoItemI): void => {
 
 /** Add TodoExpansionContent enum to the todo item when the item is expandable */
 export const addExpansionContent = (todoItem: TodoItemI): void => {
+  const filingTypeToName = useFilingTypeToName().filingTypeToName
   if (todoItem.status === FilingStatusE.DRAFT && !!todoItem.payErrorObj) {
     // if there is an incomplete payment error for a draft filing
     todoItem.expansionContent = TodoExpansionContentE.DRAFT_PAYMENT_INCOMPLETE
