aa5a7793e519edd6d56d61b17f858fcea44d1291
Séverin Beauvais <severinbeauvais@users.noreply.github.com>
2025-11-14 11:30:08 -0800

    - app version = 1.1.24 - fixed non-displaying view/hide label - fixed invalid character displayed - removed comment and commented-out code for ACTIVE check

--- src/components/bcros/filing/common/HeaderActions.vue
+++ src/components/bcros/filing/common/HeaderActions.vue
@@ -63,10 +63,7 @@
         <strong v-if="!isExpanded">{{ $t('button.filing.actions.requestACopy') }}</strong>
         <strong v-else>{{ $t('button.filing.actions.close') }}</strong>
       </template>
-      <template
-        v-else-if="isTypeStaff && !(FilingTypes.COURT_ORDER)"
-      >
-        >
+      <template v-else-if="isTypeStaff && !isFilingType(filing, FilingTypes.COURT_ORDER)">
         <strong v-if="!isExpanded">{{ $t('button.filing.actions.view') }}</strong>
         <strong v-else>{{ $t('button.filing.actions.hide') }}</strong>
       </template>
