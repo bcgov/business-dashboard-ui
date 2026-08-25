9fcbbe8c63c5b3c733b29fb3af340f1268a5a3ae
Vysakh Menon <vysakh.menon@aot-technologies.com>
2026-08-25 14:57:04 -0700

    no message

--- src/components/bcros/PartyInfo.vue
+++ src/components/bcros/PartyInfo.vue
@@ -70,16 +70,14 @@ const getName = (party: PartyI): string => {
 
   if (party.officer.organizationName) {
     name = party.officer.organizationName
-  }
-  else {
+  } else {
     name = [
       party.officer.firstName,
       party.officer.middleInitial,
       party.officer.lastName
-    ]
-    .map(x => x ? x.trim() : '')
-    .filter(x => x.length > 0)
-    .join(' ')
+    ].map(x => x ? x.trim() : '')
+      .filter(x => x.length > 0)
+      .join(' ')
   }
 
   return name
